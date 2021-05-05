61dbaa482ab828329d90a91c67e9694833a6b004
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2021-05-04 21:16:14 -0500

    team assignment02

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,8 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const ta03Controller = require('../controller/ta03');
 
-router.get('/',(req, res, next) => {
-    res.render('pages/ta03', { 
-        title: 'Team Activity 03', 
-        path: '/ta03', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
-    });
-});
+router.get('/',ta03Controller.getItems);
 
 module.exports = router;
